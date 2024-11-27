86e11e8c0f2a92c5c25c955bd158bbf89326751d
Vivek Kumar <vivek.2.sen@gmail.com>
2024-11-27 09:36:16 +0530

    change import

--- tests/register.test.ts
+++ tests/register.test.ts
@@ -1,5 +1,5 @@
 const { test, expect } = require('@playwright/test');
-const { LoginPage } = require('../Pages/LoginPage');
+const { Locators } = require('../Pages/Locators');
 import * as fs from 'fs/promises';
 
 
@@ -11,26 +11,26 @@ async function loadTestData(user: string) {
 test('Register a new user', async ({ page }) => {
   test.setTimeout(100000);
   const testData = await loadTestData('user1');
-  const loginPage = new LoginPage(page);
+    const locators = new Locators(page);
   const randomNum = Math.floor(Math.random() * 1000000);
   const testEmail = `abc+${randomNum}@example.com`;
   await fs.writeFile('./tempEmail.json', JSON.stringify({ email: testEmail }));
 
   await test.step('1. You navigate to the landing page', async () => {
-    await loginPage.navigateToLandingPage(testData.landingPageurl);
+    await locators.navigateToLandingPage(testData.landingPageurl);
   });
 
   await test.step('2. You enter the email address and click Next', async () => {
-    await loginPage.inputEmailAddress(testEmail);
+    await locators.inputEmailAddress(testEmail);
   });
 
   await test.step('3. Enter all the required fields and click submit to complete registration', async () => {
-    await loginPage.fillSignupForm(
+    await locators.fillSignupForm(
       testData.firstName,
       testData.lastName,
       testData.phoneInput,
       testData.password)
-    await loginPage.clickSubmitOnSignup();
+    await locators.clickSubmitOnSignup();
     await page.waitForEvent('domcontentloaded');
   });
 });
